7a1f0e61bd280e6f54ded60a9cf30a58a8a659b7
al_skipp <al_skipp@fastmail.fm>
2014-10-13 15:30:56 +0100

    Initial Commit

---
+++ SwiftCircleLayers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCircleLayers
+//
+//  Created by Alan Skipp on 13/10/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
